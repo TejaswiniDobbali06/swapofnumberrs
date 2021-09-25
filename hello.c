429158f598780093e003f2df4d6add2e36a0eae7
TejaswiniDobbali06 <91354919+TejaswiniDobbali06@users.noreply.github.com>
2021-09-25 06:59:24 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+# swapofnumberrs
+#include<stdio.h>
+void main()
+{
+  int a,b,temp;
+printf("Before swapping a=%d and b=%d",a,b);
+temp=a;
+a=b;
+b=temp;
+printf("After swapping a=%d and b=%d",a,b);
+}
